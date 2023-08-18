bb4776e463388bd66a1228f8ab5a4d593be1f326
Muhammad Mekayel Anik <mekayel.anik@gmail.com>
2023-08-19 01:10:02 +0600

    ProFTPDv1.38-18082023

--- stable-image-builder/proftpd.sh
+++ stable-image-builder/proftpd.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
-# Show Image Build Time
-echo "This image was build on: $(cat /usr/bin/build-timestamp)"   
-# /usr/bin/banner.sh
-echo "This Container was started on: $(date +%c)"
+/usr/bin/banner.sh
+# Show Conatiner Start Time
 # Check if Username & Password is set for Each FTP share
-if [ "${NUMBER_OF_SHARES}" -gt 1 ]; then
 # Create FTP User and assign Password, UID & GID from Environment variables for Multiple Users
 	for ((i=1; i<="${NUMBER_OF_SHARES}"; i++))
 	do
 		FTP_USER=FTP_USERNAME_${i}
 		FTP_PASS=FTP_PASSWORD_${i}
-		FTP_UID=FTP_USER_${i}_PUID
-                FTP_GID=FTP_USER_${i}_PGID
-		if [ -z "${!FTP_USER}" ] || [ -z "${!FTP_PASS}" ] || [ -z "${!FTP_UID}" ] || [ -z "${!FTP_GID}" ]; then
-    			echo "You have set NUMBER_OF_SHARES to ${NUMBER_OF_SHARES}"
-			echo "So you have to set values in each of"
-			for ((j=1; j<="${NUMBER_OF_SHARES}"; j++))
-			do
-				echo "FTP_USERNAME_${j}, FTP_PASSWORD_${j}, FTP_USER_${j}_PUID & FTP_USER_${j}_GUID"
-			done
-			echo "Exitting..."
-		        exit 1
-		fi
-		    if id "${!FTP_USER}" > /dev/null 2>&1; then
-		        :
-		    else
-                        addgroup -g ${!FTP_GID} ${!FTP_USER}
-			adduser --disabled-password --uid ${!FTP_UID} -G ${!FTP_USER} -s /bin/sh -h /home/"${!FTP_USER}" ${!FTP_USER}
-		        echo "${!FTP_USER}:${!FTP_PASS}" | chpasswd
-			chown -R "${!FTP_USER}":"${!FTP_USER}" /home/"${!FTP_USER}"
-		    fi
+			if [ -z "${!FTP_USER}" ] || [ -z "${!FTP_PASS}" ] ; then
+    				echo "You have set NUMBER_OF_SHARES to ${NUMBER_OF_SHARES}"
+				echo "So you have to set values in each of"
+				for ((j=1; j<="${NUMBER_OF_SHARES}"; j++))
+				do
+					echo "FTP_USERNAME_${j}, FTP_PASSWORD_${j}"
+				done
+				echo "Exitting..."
+		        	exit 1
+			fi
+			if id "${!FTP_USER}" > /dev/null 2>&1; then
+		        	:
+			else
+				FTP_UID=FTP_USER_${i}_PUID
+	                	FTP_GID=FTP_USER_${i}_PGID
+					if [ -z "${!FTP_UID}" ] || [ -z "${!FTP_GID}" ] ; then
+						FTP_UID=$(( 1100 + i ))
+						FTP_GID=$FTP_UID
+                        			addgroup -g ${FTP_GID} ${!FTP_USER}
+						adduser -D -u ${FTP_UID} -G ${!FTP_USER} -s /bin/sh -h /data/"${!FTP_USER}" ${!FTP_USER}
+		        			echo "${!FTP_USER}:${!FTP_PASS}" | chpasswd
+						chown -R "${!FTP_USER}":"${!FTP_USER}" /data/"${!FTP_USER}"
+					elif [ -z "${!FTP_UID}" ] ; then
+						FTP_UID=${!FTP_GID}
+                        			addgroup -g ${!FTP_GID} ${!FTP_USER}
+						adduser -D -u ${FTP_UID} -G ${!FTP_USER} -s /bin/sh -h /data/"${!FTP_USER}" ${!FTP_USER}
+		        			echo "${!FTP_USER}:${!FTP_PASS}" | chpasswd
+						chown -R "${!FTP_USER}":"${!FTP_USER}" /data/"${!FTP_USER}"
+					elif [ -z "${!FTP_GID}" ] ; then
+						FTP_GID=${!FTP_UID}
+                        			addgroup -g ${FTP_GID} ${!FTP_USER}
+						adduser -D -u ${!FTP_UID} -G ${!FTP_USER} -s /bin/sh -h /data/"${!FTP_USER}" ${!FTP_USER}
+		        			echo "${!FTP_USER}:${!FTP_PASS}" | chpasswd
+						chown -R "${!FTP_USER}":"${!FTP_USER}" /data/"${!FTP_USER}"
+					else 
+						addgroup -g ${!FTP_GID} ${!FTP_USER}
+						adduser -D -u ${!FTP_UID} -G ${!FTP_USER} -s /bin/sh -h /data/"${!FTP_USER}" ${!FTP_USER}
+		        			echo "${!FTP_USER}:${!FTP_PASS}" | chpasswd
+						chown -R "${!FTP_USER}":"${!FTP_USER}" /data/"${!FTP_USER}"
+					fi
+
+			fi
 	done
-else 
-# Create FTP User and assign Password, UID & GID from Environment variables for Single User
-	    if id "$FTP_USERNAME_1" > /dev/null 2>&1; then
-        	:
-    	    else
-    	        addgroup -g "${FTP_USER_1_PGID}" "${FTP_USERNAME_1}"
-	        adduser --disabled-password --uid "${FTP_USER_1_PUID}" -G "${FTP_USERNAME_1}" -s /bin/sh -h /home/"${FTP_USERNAME_1}" "${FTP_USERNAME_1}"
-		echo "${FTP_USERNAME_1}:${FTP_PASSWORD_1}" | chpasswd
-		chown -R "${FTP_USERNAME_1}":"${FTP_USERNAME_1}" /home/"${FTP_USERNAME_1}"
-		fi
-fi
 echo -e "Include /etc/proftpd/conf.d/*.conf\n
 DefaultServer           on\n
 Group                   ftp\n
@@ -52,7 +60,7 @@ UseIPv6                 off\n
 WtmpLog                 off\n
 RootLogin               off\n
 UseReverseDNS           off\n
-AllowOverwrite          on\n
+AllowOverwrite          $ALLOW_OVERWRITE\n
 MaxInstances            $MAX_INSTANCES\n
 MaxClients              $MAX_CLIENTS\n
 PassivePorts            4559 4564\n
@@ -60,7 +68,10 @@ ServerName              $SERVER_NAME\n
 TimesGMT                $TIMES_GMT\n
 Umask                   $LOCAL_UMASK\n
 DefaultRoot             $LOCAL_ROOT\n
+TimeoutIdle             $TIMEOUT_IDLE\n
+TimeoutNoTransfer       $TIMEOUT_NO_TRANSFER\n
+TimeoutStalled          $TIMEOUT_STALLED\n
 <Limit WRITE>\n
-  AllowAll
+  AllowAll\n
 </Limit>"  > /etc/proftpd/proftpd.conf
 exec "$@"
