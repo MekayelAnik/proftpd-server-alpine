bd098bbb0247f1c6ad94742baad99ed64a983a05
MD. MEKAYEL ANIK <32511246+MekayelAnik@users.noreply.github.com>
2024-03-01 23:42:00 +0600

    Update proftpd.sh to add PUID & PGID limits
    - Added limit to PUID & PGID from 1000~2147483647
    - Some bug fixes in Group creation

--- stable-image-builder/scripts/proftpd.sh
+++ stable-image-builder/scripts/proftpd.sh
@@ -12,140 +12,216 @@ fi
 if [[ -z "${FTP_SHARE_COMMON_GROUP_NAME}" ]]; then
 	FTP_SHARE_COMMON_GROUP_NAME='ftp-common'
 fi
+######	Function for setting up Password for each FTP Share	######
+set-password() {
+	echo "${!FTP_SHARE}:${!FTP_PASS}" | chpasswd
+}
+######	Function for creating Shares with no Given UID & GID	######
 no-uid-gid() {
 	FTP_UID=$((1100 + i))
 	if getent group ${FTP_UID} >/dev/null 2>&1; then
 		FTP_UID=$((456 + FTP_UID))
 	fi
 	adduser -D -u "${FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
+	set-password
 	chown -cR "${FTP_UID}":"${FTP_UID}" /data/"${!FTP_SHARE}"
 }
 ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP=$(echo "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" | tr '[:upper:]' '[:lower:]')
 if [[ "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" == 'true' ]] || [[ "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" == 'enabled' ]] || [[ "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" == 'enable' ]] || [[ "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" == 'yes' ]] || [[ "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" == 'ok' ]] || [[ "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" == 'y' ]] || [[ "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" == 'ya' ]] || [[ "${ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP}" == '1' ]]; then
 	ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP='true'
 fi
-for ((i = 1; i <= "${NUMBER_OF_SHARES}"; i++)); do
-	FTP_SHARE=FTP_SHARE_${i}
-	FTP_PASS=FTP_PASSWORD_${i}
-	if [[ -z "${!FTP_SHARE}" ]] || [[ -z "${!FTP_PASS}" ]]; then
-		echo "You have set NUMBER_OF_SHARES to ${NUMBER_OF_SHARES}"
-		echo "So you have to set values in each of"
-		for ((j = 1; j <= "${NUMBER_OF_SHARES}"; j++)); do
-			echo "FTP_SHARE_${j}, FTP_PASSWORD_${j}"
-		done
-		echo "Exitting..."
-		exit 1
-	fi
-	if id "${!FTP_SHARE}" >/dev/null 2>&1; then
-		:
-	else
-		FTP_UID=FTP_SHARE_${i}_PUID
-		FTP_GID=FTP_SHARE_${i}_PGID
-		FTP_CHMOD=FTP_SHARE_${i}_CHMOD
-		if [[ "$ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP" == 'true' ]]; then
-			if [[ -n "${FTP_SHARE_COMMON_GID}" ]]; then
-				if [[ "${FTP_SHARE_COMMON_GID}" =~ ^[0-9]+$ ]]; then
-					if getent group "${FTP_SHARE_COMMON_GID}" >/dev/null 2>&1; then
-						:
-					else
-						addgroup --gid "${FTP_SHARE_COMMON_GID}" "${FTP_SHARE_COMMON_GROUP_NAME}"
-					fi
-					if [[ -z "${!FTP_UID}" ]]; then
-						FTP_UID=$((1100 + i))
-						adduser -D -u "${FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
-						usermod -aG "${FTP_SHARE_COMMON_GID}" "${!FTP_SHARE}"
-						chown -cR "${FTP_UID}":"${FTP_SHARE_COMMON_GID}" /data/"${!FTP_SHARE}"
-					else
-						adduser -D -u "${!FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
-						usermod -aG "${FTP_SHARE_COMMON_GID}" "${!FTP_SHARE}"
-						chown -cR "${!FTP_UID}":"${FTP_SHARE_COMMON_GID}" /data/"${!FTP_SHARE}"
-					fi
-				else
-					echo "You have to set numeric value in FTP_SHARE_COMMON_GID. Exitting..."
-					exit 1
-				fi
-			else
-				echo "You have set value TRUE in ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP but didn't set numeric value in FTP_SHARE_COMMON_GID. Exitting..."
+######	Check if NUMBER_OF_SHARES is Valid	######
+if [[ "${NUMBER_OF_SHARES}" =~ ^[0-9]+$ ]] || [[ "${NUMBER_OF_SHARES}" -le 0 ]] || [[ "${NUMBER_OF_SHARES}" != '' ]]; then
+	if [[ "${NUMBER_OF_SHARES}" -ge 1 ]] && [[ "${NUMBER_OF_SHARES}" -le 2147482647 ]]; then
+		for ((i = 1; i <= "${NUMBER_OF_SHARES}"; i++)); do
+			FTP_SHARE=FTP_SHARE_${i}
+			FTP_PASS=FTP_PASSWORD_${i}
+			if [[ -z "${!FTP_SHARE}" ]] || [[ -z "${!FTP_PASS}" ]]; then
+				echo "You have set NUMBER_OF_SHARES to ${NUMBER_OF_SHARES}"
+				echo "So you have to set values in each of"
+				for ((j = 1; j <= "${NUMBER_OF_SHARES}"; j++)); do
+					echo "FTP_SHARE_${j}, FTP_PASSWORD_${j}"
+				done
+				echo "Exitting..."
 				exit 1
 			fi
-		else
-			if [[ -z "${!FTP_UID}" ]] && [[ -z "${!FTP_GID}" ]]; then
-				FTP_UID=$((1100 + i))
-				no-uid-gid
-			elif [[ -z "${!FTP_UID}" ]] && [[ -n "${!FTP_GID}" ]]; then
-				if [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
-					FTP_UID=$((1100 + i))
-					if getent group ${FTP_UID} >/dev/null 2>&1; then
-						FTP_UID=$((456 + FTP_UID))
-					fi
-					adduser -D -u "${FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
-					if getent group ${!FTP_GID} >/dev/null 2>&1; then
-						:
-					else
-						if [[ "${FTP_UID}" != "${!FTP_GID}" ]]; then
-							addgroup --gid "${!FTP_GID}" "${FTP_GROUP_NAME}${!FTP_GID}"
-						fi
-					fi
-					usermod -aG ${!FTP_GID} ${!FTP_SHARE}
-					chown -cR "${FTP_UID}":"${!FTP_GID}" /data/"${!FTP_SHARE}"
-				else
-					echo "Set Numeric Value in ${FTP_GID}. Otherwise skipping creation of Group ${!FTP_GID}"
-					no-uid-gid
-				fi
-			elif [[ -n "${!FTP_UID}" ]] && [[ -z "${!FTP_GID}" ]]; then
-				if [[ "${!FTP_UID}" =~ ^[0-9]+$ ]]; then
-					if getent passwd ${!FTP_UID} >/dev/null 2>&1; then
-						echo "User with same UID already exists. Creating user with different UID"
-						no-uid-gid
-					else
-						adduser -D -u "${!FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
-					fi
-				else
-					echo "UID of ${!FTP_SHARE} is not Numeric. Change the value of ${FTP_UID} to a numeric one. Otherwise skipping creation of ${!FTP_SHARE}..."
-				fi
-			elif [[ -n "${!FTP_UID}" ]] && [[ -n "${!FTP_GID}" ]]; then
-				if [[ "${!FTP_UID}" =~ ^[0-9]+$ ]]; then
-					if [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
-						if getent group ${!FTP_GID} >/dev/null 2>&1; then
-							:
-						else
-							if [[ "${!FTP_UID}" != "${!FTP_GID}" ]]; then
-								addgroup --gid "${!FTP_GID}" "${FTP_GROUP_NAME}${!FTP_GID}"
+			if id "${!FTP_SHARE}" >/dev/null 2>&1; then
+				:
+			else
+				FTP_UID=FTP_SHARE_${i}_PUID
+				FTP_GID=FTP_SHARE_${i}_PGID
+				FTP_CHMOD=FTP_SHARE_${i}_CHMOD
+				if [[ "$ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP" == 'true' ]]; then
+					######	If the Share is not under a COMMON FTP GROUP	######
+					if [[ -n "${FTP_SHARE_COMMON_GID}" ]]; then
+						if [[ "${FTP_SHARE_COMMON_GID}" =~ ^[0-9]+$ ]]; then
+							if [[ "${FTP_SHARE_COMMON_GID}" -ge 1000 ]] && [[ "${FTP_SHARE_COMMON_GID}" -le 2147483647 ]]; then
+								if getent group "${FTP_SHARE_COMMON_GID}" >/dev/null 2>&1; then
+									:
+								else
+									addgroup --gid "${FTP_SHARE_COMMON_GID}" "${FTP_SHARE_COMMON_GROUP_NAME}"
+								fi
+								if [[ -z "${!FTP_UID}" ]]; then
+									FTP_UID=$((1100 + i))
+									adduser -D -u "${FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
+									set-password
+									usermod -aG "${FTP_SHARE_COMMON_GID}" "${!FTP_SHARE}"
+									chown -cR "${FTP_UID}":"${FTP_SHARE_COMMON_GID}" /data/"${!FTP_SHARE}"
+								else
+									if [[ "${!FTP_UID}" =~ ^[0-9]+$ ]]; then
+										if [[ "${!FTP_UID}" -ge 1000 ]] && [[ "${!FTP_UID}" -le 2147483647 ]]; then
+											adduser -D -u "${!FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
+											set-password
+											usermod -aG "${FTP_SHARE_COMMON_GID}" "${!FTP_SHARE}"
+											chown -cR "${!FTP_UID}":"${FTP_SHARE_COMMON_GID}" /data/"${!FTP_SHARE}"
+										else
+											echo "The PUID for ${!FTP_SHARE} is out of optimal range. Set unique PUID value between 1000 to 2147483647 in $FTP_UID. Otherwise skipping creation of ${!FTP_SHARE}..."
+										fi
+									else
+										echo "You have to set integer value in FTP_SHARE_COMMON_GID. Otherwise skipping the creation of ${!FTP_SHARE}..."
+									fi
+								fi
+							else
+								echo "The GID for ${FTP_SHARE_COMMON_GROUP_NAME} is set to ${FTP_SHARE_COMMON_GID}, which is out of optimal range. Set unique GUID value between 1000 to 2147483647 in FTP_SHARE_COMMON_GID. Otherwise Exitting..."
+								exit 1
 							fi
+						else
+							echo "You have to set integer value in FTP_SHARE_COMMON_GID. Exitting..."
+							exit 1
 						fi
 					else
-						echo "GUID of ${!FTP_SHARE} is not Numeric. Skipping Group Creation..."
+						echo "You have set value TRUE in ALL_FTP_SHARES_UNDER_SINGLE_COMMON_GROUP but didn't set integer value in FTP_SHARE_COMMON_GID. Otherwise Exitting..."
+						exit 1
 					fi
-					if getent passwd ${!FTP_UID} >/dev/null 2>&1; then
-						echo "User with same UID already exists."
-						FTP_UID=$((1100 + i))
-						if getent group ${FTP_UID} >/dev/null 2>&1; then
-							FTP_UID=$((456 + FTP_UID))
-						fi
-						echo "Creating FTP USER with UID=$FTP_UID"
-						adduser -D -u "${FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
-						if [[ "${FTP_UID}" != "${!FTP_GID}" ]]; then
-							usermod -aG ${!FTP_GID} ${!FTP_SHARE}
-						fi
-						chown -cR "${FTP_UID}":"${!FTP_GID}" /data/"${!FTP_SHARE}"
-					else
-						adduser -D -u "${!FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
-						if [[ "${!FTP_UID}" != "${!FTP_GID}" ]]; then
-							usermod -aG ${!FTP_GID} ${!FTP_SHARE}
-						fi
-						chown -cR "${!FTP_UID}":"${!FTP_GID}" /data/"${!FTP_SHARE}"
-					fi
+				######	If the Share is not under a COMMON FTP GROUP	######
 				else
-					echo "UID of ${!FTP_SHARE} is not Numeric. Change the value of ${FTP_UID} to a numeric one. Otherwise skipping creation of ${!FTP_SHARE}..."
+					######	Creating FTP Shares with no Given UID & GID	######
+					if [[ -z "${!FTP_UID}" ]] && [[ -z "${!FTP_GID}" ]]; then
+						FTP_UID=$((1100 + i))
+						no-uid-gid
+					######	Creating FTP Shares with Given UID & but no Given GID	######
+					elif [[ -n "${!FTP_UID}" ]] && [[ -z "${!FTP_GID}" ]]; then
+						if [[ "${!FTP_UID}" =~ ^[0-9]+$ ]]; then
+							if [[ "${!FTP_UID}" -ge 1000 ]] && [[ "${!FTP_UID}" -le 2147483647 ]]; then
+								if getent passwd ${!FTP_UID} >/dev/null 2>&1; then
+									echo "User with same PUID already exists. Creating user with different PUID"
+									no-uid-gid
+								else
+									adduser -D -u "${!FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
+									set-password
+								fi
+							else
+								echo "The PUID for ${!FTP_SHARE} is set to ${!FTP_UID}, which is out of optimal range. Set unique PUID value between 1000 to 2147483647 in $FTP_UID. Otherwise skipping creation of ${!FTP_SHARE}..."
+							fi
+						else
+							echo "UID of ${!FTP_SHARE} is not integer. Change the value of ${FTP_UID} to a integer one. Otherwise skipping creation of ${!FTP_SHARE}..."
+						fi
+					######	Creating FTP Shares with no Given UID &  but Given GID	######
+					elif [[ -z "${!FTP_UID}" ]] && [[ -n "${!FTP_GID}" ]]; then
+						if [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
+							if [[ "${!FTP_GID}" -ge 1000 ]] && [[ "${!FTP_GID}" -le 2147483647 ]]; then
+								if getent passwd ${!FTP_GID} >/dev/null 2>&1; then
+									FTP_UID=$((1100 + i))
+								else
+									FTP_UID="${!FTP_GID}"
+								fi
+								if getent group "${FTP_UID}" >/dev/null 2>&1; then
+									FTP_UID=$((456 + FTP_UID))
+								fi
+								adduser -D -u "${FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
+								set-password
+								if getent group "${!FTP_GID}" >/dev/null 2>&1; then
+									:
+								else
+									if [[ "${FTP_UID}" != "${!FTP_GID}" ]]; then
+										addgroup --gid "${!FTP_GID}" "${FTP_GROUP_NAME}${!FTP_GID}"
+									fi
+								fi
+								if getent group ${!FTP_GID} >/dev/null 2>&1; then
+									usermod -aG "${!FTP_GID}" "${!FTP_SHARE}"
+								else
+									echo "Group with GUID=${!FTP_GID} doesn't Exist. Hence ${!FTP_SHARE} share is not added to Group with GUID=${!FTP_GID}"
+								fi
+								chown -cR "${FTP_UID}":"${!FTP_GID}" /data/"${!FTP_SHARE}"
+							else
+								echo "The GUID for ${!FTP_SHARE} is set to ${!FTP_GID}, which is out of optimal range. Set unique GUID value between 1000 to 2147483647 in $FTP_GID. Otherwise Skipping group creation..."
+							fi
+						else
+							echo "Set integer Value in ${FTP_GID}. Otherwise skipping creation of Group ${!FTP_GID}"
+							no-uid-gid
+						fi
+					######	Creating FTP Shares with Given UID & Given GID	######
+					elif [[ -n "${!FTP_UID}" ]] && [[ -n "${!FTP_GID}" ]]; then
+						if [[ "${!FTP_UID}" =~ ^[0-9]+$ ]]; then
+							if [[ "${!FTP_UID}" -ge 1000 ]] && [[ "${!FTP_UID}" -le 2147483647 ]]; then
+								if [[ "${!FTP_UID}" != "${!FTP_GID}" ]]; then
+									if [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
+										if [[ "${!FTP_GID}" -ge 1000 ]] && [[ "${!FTP_GID}" -le 2147483647 ]]; then
+											if getent group "${!FTP_GID}" >/dev/null 2>&1; then
+												echo "Group already exists of GUID=${!FTP_GID}. So skipping this group creation..."
+											else
+												addgroup --gid "${!FTP_GID}" "${FTP_GROUP_NAME}${!FTP_GID}"
+											fi
+										else
+											echo "The GUID for ${!FTP_SHARE} is set to ${!FTP_GID}, which is out of optimal range. Set unique GUID value between 1000 to 2147483647 in $FTP_GID. Otherwise Skipping Group creation..."
+										fi
+									else
+										echo "GUID of ${!FTP_SHARE} is not integer. Skipping Group Creation..."
+									fi
+								fi
+								if getent passwd ${!FTP_UID} >/dev/null 2>&1; then
+									echo "Share with PUID=${!FTP_UID} already exists."
+									FTP_UID=$((1100 + i))
+									if getent group "${FTP_UID}" >/dev/null 2>&1; then
+										FTP_UID=$((456 + FTP_UID))
+									fi
+									echo "Instead creating FTP Share ${!FTP_SHARE} with UID=${FTP_UID} and assigning it to the PGID=${!FTP_GID}"
+									adduser -D -u "${FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
+									set-password
+									if [[ "${FTP_UID}" != "${!FTP_GID}" ]] && [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
+										usermod -aG "${!FTP_GID}" "${!FTP_SHARE}"
+									fi
+									if [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
+										chown -cR "${FTP_UID}":"${!FTP_GID}" /data/"${!FTP_SHARE}"
+									fi
+								else
+									adduser -D -u "${!FTP_UID}" -s /bin/sh -h /data/"${!FTP_SHARE}" "${!FTP_SHARE}"
+									set-password
+									if [[ "${!FTP_UID}" != "${!FTP_GID}" ]]; then
+										if getent group ${!FTP_GID} >/dev/null 2>&1 && [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
+											usermod -aG "${!FTP_GID}" "${!FTP_SHARE}"
+										elif [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
+											echo "Group with GUID=${!FTP_GID} doesn't Exist. Hence ${!FTP_SHARE} share is not added to Group with GUID=${!FTP_GID}"
+										elif ! [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
+											echo "GUID of ${!FTP_SHARE} is not an integer value. Hence ${!FTP_SHARE} share is not added to Group with GUID=${!FTP_GID}"
+										fi
+									fi
+									if [[ "${!FTP_GID}" =~ ^[0-9]+$ ]]; then
+										chown -cR "${!FTP_UID}":"${!FTP_GID}" /data/"${!FTP_SHARE}"
+									fi
+								fi
+							else
+								echo "The PUID for ${!FTP_SHARE} is set to ${!FTP_UID}, which out of optimal range. Set unique PUID value between 1000 to 2147483647 in $FTP_UID. Otherwise skipping creation of ${!FTP_SHARE}..."
+							fi
+						else
+							echo "UID of ${!FTP_SHARE} is not integer. Change the value of ${FTP_UID} to a integer one. Otherwise skipping creation of ${!FTP_SHARE}..."
+						fi
+					fi
+				fi
+				######	CHMOD an FTP Share if the is a valid value	######
+				if [[ -n "${!FTP_CHMOD}" ]] && [[ "${!FTP_CHMOD}" =~ ^[0-9]+$ ]] && [[ "${!FTP_CHMOD}" -ge 0 ]] && [[ "${!FTP_CHMOD}" -le 0777 ]]; then
+					chmod -cR "${!FTP_CHMOD}" /data/"${!FTP_SHARE}"
 				fi
 			fi
-		fi
-		echo "${!FTP_SHARE}:${!FTP_PASS}" | chpasswd
-		if [[ -n "${!FTP_CHMOD}" ]] && [[ "${!FTP_CHMOD}" =~ ^[0-9]+$ ]]; then
-			chmod -cR "${!FTP_CHMOD}" /data/"${!FTP_SHARE}"
-		fi
+		done
+	else
+		echo "The NUMBER_OF_SHARES is set to ${NUMBER_OF_SHARES}, which is out of optimal range. Set value between 1000 to 2147483647 in NUMBER_OF_SHARES. Otherwise Exitting..."
+		exit 1
 	fi
-done
+else
+	echo "Set a integer Greater than 0 and less than 2147482647 value in NUMBER_OF_SHARES. Otherwise No Share will be created. Exitting..."
+	exit 1
+fi
 echo -e "Include /etc/proftpd/conf.d/*.conf\n
 DefaultServer           on\n
 Group                   ftp\n
